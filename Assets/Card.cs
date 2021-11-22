b41eb70f1901a4b1f59e0c2f935819566da2dcbe
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2021-11-22 18:52:54 -0300

    Card configuration and setup

---
+++ Assets/Card.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public Card_ myCardTemplate;
+    public void Init()
+    {
+
+    }
+}
